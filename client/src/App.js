fd00e17be5b519119b772669317d74fae189d58b
Nancy Lewis <nancytranlewis@gmail.com>
2021-04-25 15:48:00 -0700

    Added the parallax scrolling effect

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,12 @@ import Library from './components/Library';
 import Details from './components/Details';
 import {Router} from '@reach/router';
 
+
 const code = new URLSearchParams(window.location.search).get("code");
 
 // const oauthToken = 'BQD_OEa3K12JwEwBgFxuEDoQVfoOuubn6Oex4A7UYGFbFT_qAhVX_AFDBdpvRRqFuUp77sNhoHmzK-RoTnv2lEbpNpt8cfXYN2U2u-JFNmIMINpKmgg_fe7q5psnhpq2tyDmcftoFns4ucMOJwgpd7RCzwMN5N6aJG7wb4hV4XJBhkEAlwAxoH67Le5-nZD7SGGAPaHDIDhQSQvYr4WBESt2F0xvpkpijCsNnXjMtLwzIhgt8J6puig_yS113ZPeBgatLL6IDub7yNzyDiC09u4m9rtBLUSJydjJ_LcwqYOk';
 
-
 function App() {
-
   return (
 
     <div className="App">
@@ -19,10 +18,7 @@ function App() {
             <Library code={code} path="/library" />
             <Details code={code} path="/details/:_id" />
           </Router>
-
     </div>
-    
-
 
       /* <div className="App">
           <Details code={oauthToken} id='3n3Ppam7vgaVa1iaRUc9Lp'/>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Parallax } from 'react-parallax';
+import acousticGuitar from '../images/acoustic_guitar.jpg'; //strength:700
+import cheers2 from '../images/cheers2.jpg'; //strength: 400
+import guitar from '../images/guitar.jpg'; //strength: 600
+import turntable from '../images/turntable.jpg'; //strength: 600
 
 
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=3c1ea11d29eb442c95c650380ba9f81b&response_type=code&redirect_uri=http://localhost:3000/library&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state"
+
+const insideStyles = {
+    background: "#fff", 
+    left: '50%', 
+    top: '50%', 
+    position: 'absolute', 
+    padding: '20px',
+    transform: 'translate(-50%, -50%)',
+}
 
 const Home = () => {
 
@@ -17,16 +31,16 @@ const Home = () => {
                 </div>
                 
             </div>
-
-            {/*  Insert Image here */}
-            {/*  Add parallax effect here  */}
-
-            <div style={{"marginTop": "15%"}}>
-                <h3>Join us for an exceptional music experience</h3>
-
-                {/*  Added button as placeholder */}
-                <a href={AUTH_URL}><button>Get Started</button></a> 
-            </div>
+            
+            <Parallax bgImage={ guitar } bgImageAlt="guitar" strength={600}>
+                <div style={{height: 1000}}>
+                    <div style={insideStyles}>
+                        <h1>Join us for an exceptional music experience</h1>
+                        <a href={AUTH_URL}><button style={{"border": "1px solid peachpuff", "borderRadius": "5px", "width": "200px", "height": "40px", "backgroundColor": "SandyBrown", "color": "whitesmoke", "fontWeight": "bold", "fontSize": "16px"}}>GET STARTED</button></a> 
+                    </div>
+                </div>
+            </Parallax>
+            <div style={{"height": "50vh"}}></div>
         </div>
     )
 }
