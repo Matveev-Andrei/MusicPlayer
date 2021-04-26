962b96c0a983b411259a9c4558930b6a9207d296
Mike Le <mike.le@live.com>
2021-04-25 21:10:10 -0700

    added and tested login, refresh and create

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,80 @@
+require('dotenv').config();
+const User = require('../models/user.model');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+module.exports = {
+    login: (req, res) => {
+        const code = req.body.code;
+
+        const spotifyApi = new SpotifyWebApi({
+            redirectUri: 'http://localhost:3000',
+            clientId: process.env.SPOTIFY_CLIENT_ID,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+        });
+
+        spotifyApi.authorizationCodeGrant(code)
+            .then(data => {
+                res.json({
+                    accessToken: data.body.access_token,
+                    refreshToken: data.body.refresh_token,
+                    expiresIn: data.body.expires_in
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                res.sendStatus(400)
+            });
+    },
+
+    refresh: (req, res) => {
+        const refreshToken = req.body.refreshToken;
+
+        const spotifyApi = new SpotifyWebApi({
+            redirectUri: 'http://localhost:3000',
+            clientId: process.env.SPOTIFY_CLIENT_ID,
+            clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+            refreshToken
+        });
+
+        spotifyApi.refreshAccessToken()
+            .then((data) => {
+                res.json({
+                    accessToken: data.body.accessToken,
+                    expiresIn: data.body.expiresIn
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+                res.sendStatus(400);
+            });
+    },
+
+    create: (req, res) => {
+        
+        const spotifyApi = new SpotifyWebApi();
+        spotifyApi.setAccessToken(req.body.accessToken);
+        
+        (async () => {
+            const user = await spotifyApi.getMe();
+            console.log(user.body);
+            
+            User.create({
+                username: user.body.id,
+                email: user.body.email,
+                display_name: user.body.display_name,
+            })
+                .then((newUser) => {
+                    res.json(newUser);
+                })
+                .catch((err) => {
+                    console.log("Error found in create");
+                    console.log(err);
+                    res.json(err);
+                })
+        })()
+            .catch(err => {
+                console.error(err);
+            });
+
+    }
+}
