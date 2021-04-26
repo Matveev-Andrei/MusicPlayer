2f892b4bb298782b2c39ea3be31050bdf78dc65c
Mike Le <mike.le@live.com>
2021-04-25 21:30:45 -0700

    add useAuth hook to frontend and create a user when login

--- client/src/components/Library.js
+++ client/src/components/Library.js
@@ -1,7 +1,37 @@
+import React, {useState, useEffect} from 'react';
 import {Link} from '@reach/router';
+import useAuth from './useAuth';
+import SpotifyWebApi from 'spotify-web-api-node';
+import axios from 'axios';
 
+const spotifyApi = new SpotifyWebApi({
+    clientId: "3c1ea11d29eb442c95c650380ba9f81b"
+})
 
 const Library = (props) => {
+
+    const accessToken = useAuth(props.code);
+
+    useEffect(() => {
+        if(!accessToken) return;
+        spotifyApi.setAccessToken(accessToken);
+    }, [ accessToken ]);
+
+    // this is to create a user
+    useEffect(() => {
+        if(!accessToken) return 
+        
+        axios.post('http://localhost:3001/create', {
+            accessToken
+        })
+        .then(res => {
+            console.log("create successful");
+        })
+        .catch((err) => {
+            console.log("Error found when creating user", err);
+        });
+    }, [accessToken])
+
 
     return (
         <div>
