1088754e8060b7c4e9590cd9f65ea096f51f4548
Mike Le <mike.le@live.com>
2021-04-25 21:11:59 -0700

    user routes work with login, refresh, create

---
+++ server/routes/user.routes.js
@@ -0,0 +1,7 @@
+const UserController = require('../controllers/user.controller');
+
+module.exports = (app) => {
+    app.post('/login', UserController.login);
+    app.post('/refresh', UserController.refresh);
+    app.post('/create', UserController.create);
+}
